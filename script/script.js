0057e678f87ec68d9d741b3dce64d42ab7ca4cd7
pennjay237 <pennjude31@gmail.com>
2025-05-22 12:03:42 +0100

    html and styling

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("transaction-form");
+const descriptionInput = document.getElementById("description");
+const amountInput = document.getElementById("amount");
+const transactionList = document.getElementById("transaction-list");
+const balanceEl = document.getElementById("balance");
+const incomeEl = document.getElementById("income");
+const expenseEl = document.getElementById("expense");
+const filterButtons = document.querySelectorAll(".filter");
+
